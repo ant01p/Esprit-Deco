6e050ca6082a765a209ef6ec0308395fbe8433c5
ant01p <ponvienne.antoine@gmail.com>
2026-05-07 10:09:25 +0200

    lot2-4:refacto AdminProductController-ImageHandler

--- app/src/Controller/AdminProductController.php
+++ app/src/Controller/AdminProductController.php
@@ -7,37 +7,29 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Entity\Product;
 use App\Form\ProductType;
-use App\Service\ImageHandler;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
-use Symfony\Component\String\Slugger\SluggerInterface;
+use App\Service\ImageHandler;
+
 
 final class AdminProductController extends AbstractController
 {
     #[Route('/admin/product/new', name: 'admin_product_new')]
-    public function addProduct(
-        Request $request,
-        EntityManagerInterface $entityManager,
-        SluggerInterface $slugger,
-        ImageHandler $imageHandler
-    ): Response {
-
+    public function new(Request $request, EntityManagerInterface $entityManager, ImageHandler $imageHandler): Response
+    {
         $product = new Product();
-        
+
         $form = $this->createForm(ProductType::class, $product);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            
             $imageFile = $form->get('imageFile')->getData();
-        
+
             if ($imageFile) {
 
-                $image = $imageHandler->uploadFiles(
-                    $product,
-                    $imageFile,
-                    $slugger
-                );
+                $image = $imageHandler->uploadFiles($imageFile);
+                $image->setAlt($product->getTitle());
+                $product->addImage($image);
 
                 $entityManager->persist($image);
             }
@@ -45,13 +37,13 @@ final class AdminProductController extends AbstractController
             $entityManager->persist($product);
             $entityManager->flush();
 
-            $this->addFlash('success','Le produit a bien été ajouté.' );
+            $this->addFlash('success', 'Le produit a bien été ajouté.');
 
             return $this->redirectToRoute('admin_product_index');
         }
 
         return $this->render('admin/product/new-product.html.twig', [
-            'form' => $form,
+            'form' => $form->createView(),
         ]);
     }
 }
--- app/src/Service/ImageHandler.php
+++ app/src/Service/ImageHandler.php
@@ -3,6 +3,7 @@ namespace App\Service;
 
 use App\Entity\Product;
 use App\Entity\Image;
+use App\Repository\ProductRepository;
 use Symfony\Component\String\Slugger\SluggerInterface;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 use Symfony\Component\HttpFoundation\RequestStack;
@@ -11,11 +12,13 @@ class ImageHandler
 {
     private string $projectDir;
     private RequestStack $requestStack;
+    private SluggerInterface $slugger;
 
-    public function __construct(string $projectDir, RequestStack $requestStack)
+    public function __construct(string $projectDir, RequestStack $requestStack, SluggerInterface $slugger)
     {
         $this->projectDir = $projectDir;
         $this->requestStack = $requestStack;
+        $this->slugger = $slugger;
     }
 
     public function deleteFiles(Product $product): void
@@ -29,15 +32,11 @@ class ImageHandler
         }
     }
 
-    public function uploadFiles(
-        Product $product, 
-        UploadedFile $imageFile, 
-        SluggerInterface $slugger
-    ): Image
+    public function uploadFiles( UploadedFile $imageFile,): Image  
+       
     {
-        
         $originalFilename = pathinfo($imageFile->getClientOriginalName(), PATHINFO_FILENAME);
-        $safeFilename = $slugger->slug($originalFilename);
+        $safeFilename = $this->slugger->slug($originalFilename);
         $newFilename = $safeFilename . '-' . uniqid() . '.' . $imageFile->guessExtension();
 
         try {
@@ -57,11 +56,8 @@ class ImageHandler
         $image = new Image();
 
         $image->setPath('images/product/' . $newFilename);
-        $image->setAlt($product->getTitle());
         $image->setIsPrincipal(true);
-        $image->setProduct($product);
 
         return $image;
-
     }
 }
